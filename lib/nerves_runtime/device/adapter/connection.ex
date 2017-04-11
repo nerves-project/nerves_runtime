d5fe649b4ce5c5ac7a7ab89083ecaee0d9214b94
Justin Schneck <jschneck@mac.com>
2017-04-11 12:19:35 -0400

    Remove hardware abstraction layer until refined (#7)

--- lib/nerves_runtime/device/adapter/connection.ex
+++
@@ -1,6 +0,0 @@
-defmodule Nerves.Runtime.Device.Adapter.Connection do
-
-  @callback handle_data_in(device :: Device.t, data :: term, state :: term) ::
-    {:noreply, state :: term} |
-    {:disconnect, state :: term}
-end
