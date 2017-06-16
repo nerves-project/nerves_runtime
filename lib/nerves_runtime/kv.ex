a9decfe25c609810bdc43a9bf88bb2483b6ebc54
Justin Schneck <jschneck@mac.com>
2017-06-15 23:39:49 -0400

    added docs for kv

--- lib/nerves_runtime/kv.ex
+++ lib/nerves_runtime/kv.ex
@@ -1,26 +1,65 @@
 defmodule Nerves.Runtime.KV do
+  @moduledoc """
+  Key Value Storage for firmware vairables provided by fwup
+
+  KV provides access to metadata variables set by fwup.
+  It can be used to obtain information such as the active
+  firmware slot, where the application data partition
+  is located, etc.
+
+  Values are stored in two ways.
+  * Values do not pertain to a specific firmware slot
+  For example:
+    `"nerves_fw_active" => "a"`
+
+  * Values pretain to a specific firmware slot
+  For Example:
+    `"a.nerves_fw_author" => "The Nerves Team"`
+
+  You can find values for just the active firmware slot by
+  using get_active and get_all_active. The result of these
+  functions will trim the firmware slot "a." or "b." off the
+  from the leading characters of the keys returned.
+  """
   use GenServer
   require Logger
 
+  @doc """
+  Start the KV store server
+  """
   def start_link(kv \\ "") do
     GenServer.start_link(__MODULE__, kv, name: __MODULE__)
   end
 
+  @doc """
+  Get the key for only the active firmware slot
+  """
   def get_active(key) do
     GenServer.call(__MODULE__, {:get_active, key})
   end
 
+  @doc """
+  get the key regardless of firmware slot
+  """
   def get(key) do
     GenServer.call(__MODULE__, {:get, key})
   end
 
+  @doc """
+  Get all key value pairs for only the active firmware slot
+  """
   def get_all_active() do
     GenServer.call(__MODULE__, :get_all_active)
   end
 
+  @doc """
+  Get all keys regardless of firmware slot
+  """
   def get_all() do
     GenServer.call(__MODULE__, :get_all)
   end
+
+  # GenServer API
 
   def init(_kv) do
     exec = System.find_executable("fw_printenv")
