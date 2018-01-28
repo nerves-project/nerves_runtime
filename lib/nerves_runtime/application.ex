73491951755a78dd0ac12a58f4f1f164373b533c
Greg Mefford <greg@gregmefford.com>
2018-01-28 12:40:09 -0500

    Remove boilerplate and update recommended version spec in README

--- lib/nerves_runtime/application.ex
+++ lib/nerves_runtime/application.ex
@@ -1,6 +1,4 @@
 defmodule Nerves.Runtime.Application do
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,15 +6,12 @@ defmodule Nerves.Runtime.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
     children = [
       supervisor(Nerves.Runtime.Kernel, []),
       worker(Nerves.Runtime.KV, []),
       worker(Nerves.Runtime.Init, [])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Nerves.Runtime.Supervisor]
     Supervisor.start_link(children, opts)
   end
