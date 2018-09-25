ea804f9326c649681e3fcf72e8dd81d26a6508b6
Justin Schneck <jschneck@mac.com>
2018-09-25 10:34:38 -0400

    v0.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nerves.Runtime.MixProject do
   def project do
     [
       app: :nerves_runtime,
-      version: "0.6.5",
+      version: "0.7.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       make_clean: ["clean"],
