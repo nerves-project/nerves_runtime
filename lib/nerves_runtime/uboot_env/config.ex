a9a88fd02467c1dfbd3054c87120c8d1b2405c77
Frank Hunleth <fhunleth@troodon-software.com>
2018-09-28 19:33:04 -0400

    Dialyzer fixes

--- lib/nerves_runtime/uboot_env/config.ex
+++ lib/nerves_runtime/uboot_env/config.ex
@@ -1,18 +1,19 @@
 defmodule Nerves.Runtime.UBootEnv.Config do
   @default_config_file "/etc/fw_env.config"
 
-  @spec read(file :: binary) ::
-          {:ok, {dev_name :: binary, dev_offset :: non_neg_integer, env_size :: non_neg_integer}}
+  @spec read(Path.t()) ::
+          {:ok,
+           {dev_name :: String.t(), dev_offset :: non_neg_integer(), env_size :: pos_integer()}}
           | {:error, reason :: any}
-  def read(config_file \\ nil) do
-    config_file = config_file || @default_config_file
-
+  def read(config_file \\ @default_config_file) do
     case File.read(config_file) do
       {:ok, config} -> {:ok, decode(config)}
       _error -> {:error, :no_config}
     end
   end
 
+  @spec decode(String.t()) ::
+          {dev_name :: String.t(), dev_offset :: non_neg_integer(), env_size :: pos_integer()}
   def decode(config) do
     [config] =
       config
