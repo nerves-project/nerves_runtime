7bcc10b63fa13d111494607cf2c68e68b39b3531
Frank Hunleth <fhunleth@troodon-software.com>
2018-10-01 11:45:16 -0400

    Sync common project files with Elixir 1.7 style

--- mix.exs
+++ mix.exs
@@ -20,9 +20,7 @@ defmodule Nerves.Runtime.MixProject do
 
   def application do
     [
-      extra_applications: [
-        :logger
-      ],
+      extra_applications: [:logger],
       mod: {Nerves.Runtime.Application, []}
     ]
   end
