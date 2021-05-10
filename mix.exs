ec02f8b9c3777a73d99ab80395493a74408ea287
Frank Hunleth <fhunleth@troodon-software.com>
2021-05-10 17:24:06 -0400

    Fix undefined reference warnings on the changelog

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Nerves.Runtime.MixProject do
       extras: ["README.md", "CHANGELOG.md"],
       main: "readme",
       source_ref: "v#{@version}",
-      source_url: @source_url
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
     ]
   end
 
