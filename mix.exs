7e9346dd8333e1817359c97a82c116278c2c71fb
Frank Hunleth <fhunleth@troodon-software.com>
2024-12-14 19:03:35 -0500

    Require Elixir 1.13 and bump CI
    Even though Nerves.Runtime still works with Elixir 1.11 and 1.12, it's
    almost impossible to use with those versions any more. Plus the Nerves
    core team hasn't used either of those in forever.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Nerves.Runtime.MixProject do
     [
       app: :nerves_runtime,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
