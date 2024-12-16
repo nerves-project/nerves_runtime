bdf1a04376f80a7597f97f86e9c9519433fa613f
Frank Hunleth <fhunleth@troodon-software.com>
2024-12-16 07:23:26 -0500

    Use ops.fw for firmware validation
    This falls back to the current way in case the ops.fw code isn't
    working. A warning is logged, since this really should be fixed.

--- lib/nerves_runtime.ex
+++ lib/nerves_runtime.ex
@@ -120,11 +120,15 @@ defmodule Nerves.Runtime do
   """
   @spec validate_firmware() :: :ok
   def validate_firmware() do
-    # If using U-Boot's bootcount feature, set those variables as well
-    if KV.get("upgrade_available") do
-      KV.put(%{"upgrade_available" => "0", "bootcount" => "0", "nerves_fw_validated" => "1"})
-    else
-      KV.put("nerves_fw_validated", "1")
+    with {:error, reason} <- FwupOps.validate() do
+      Logger.error("Using old validation logic due to ops.fw error: #{inspect(reason)}")
+
+      # If using U-Boot's bootcount feature, set those variables as well
+      if KV.get("upgrade_available") do
+        KV.put(%{"upgrade_available" => "0", "bootcount" => "0", "nerves_fw_validated" => "1"})
+      else
+        KV.put("nerves_fw_validated", "1")
+      end
     end
   end
 
